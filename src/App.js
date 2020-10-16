9cc22ec3113a8ced6066f2dccebb799425beb894
Anthony Lalande <anthony.lalande@gmail.com>
2020-10-15 23:26:09 -0400

    Remove story-1. Wasn't funny...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import { Intro } from './cards/intro';
 import { SelfIdentification } from './cards/self-id';
-import { StoryPart1 } from './cards/story-1';
 import { StoryPart2 } from './cards/story-2';
 import { GenerateObservations } from './cards/observations';
 
@@ -17,7 +16,6 @@ class App extends Component {
         <HashRouter>
           <Switch>
             <Route path="/who_do_you_think_you_are_and_how_dare_you"><SelfIdentification/></Route>
-            <Route path="/ok_whatever"><StoryPart1/></Route>
             <Route path="/hows_business"><StoryPart2/></Route>
             <Route path="/observations"><GenerateObservations/></Route>
             <Route><Intro /></Route>
